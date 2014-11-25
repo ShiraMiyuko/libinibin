6d2f772005697cd48e75c000dc236083ec8c99d1
Michael Yu <ItzWarty@gmail.com>
2014-11-25 15:55:15 -0800

    Pull kReadersByDataType lookup out of foreach to prevent wasteful lookups.

--- InibinLoader.cs
+++ InibinLoader.cs
@@ -125,8 +125,9 @@ namespace Dargon.LeagueOfLegends {
       /// <param name="section">The data type of the values</param>
       private static void ReadValues(BinaryReader reader, Inibin inibin, InibinTroybinDataSection section) {
          var keys = ReadKeys(reader);
+         var readValue = kReadersByDataType[section];
          foreach (var key in keys) {
-            object value = kReadersByDataType[section](reader);
+            object value = readValue(reader);
             inibin.Set(key, value);
          }
       }
